e63407698e1e89f9d4e139d5f2b38134d72bd5f2
Cosmin Iulian <cosmin.iulian03@gmail.com>
2021-06-28 21:37:51 +0300

    first commit

---
+++ MultiLanguage/MultiLanguage/ML.swift
@@ -0,0 +1,25 @@
+//
+//  ML.swift
+//  MultiLanguage
+//
+//  Created by Cosmin Iulian on 27.06.2021.
+//
+
+import Foundation
+
+
+public class ML {
+    
+    public static func getString(for key: String) -> String {
+        
+        return NSLocalizedString(
+            key,
+            tableName: nil,
+            bundle: Bundle(for: self),
+            value: key,
+            comment: ""
+        )
+    }
+    
+}
+
---
+++ MultiLanguage/MultiLanguage/MultiLanguage.h
@@ -0,0 +1,18 @@
+//
+//  MultiLanguage.h
+//  MultiLanguage
+//
+//  Created by Cosmin Iulian on 27.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MultiLanguage.
+FOUNDATION_EXPORT double MultiLanguageVersionNumber;
+
+//! Project version string for MultiLanguage.
+FOUNDATION_EXPORT const unsigned char MultiLanguageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiLanguage/PublicHeader.h>
+
+
